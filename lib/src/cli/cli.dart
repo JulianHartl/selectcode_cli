135668e2d6bac4aa84bda78aa85c6864af5145b0
Julian Hartl <julian.hartl.dev@gmail.com>
2022-10-16 21:05:43 +0200

    change gitignore

--- lib/src/cli/cli.dart
+++ lib/src/cli/cli.dart
@@ -64,7 +64,7 @@ abstract class _Cli {
 
     logger
       ..detail("stdout:\n${result.stdout}")
-      ..detail("stderr:\n${result.stderr}");
+      ..info("stderr:\n${result.stderr}");
     if (!result.isSuccess) {
       if (throwError) {
         throw ProcessException(
